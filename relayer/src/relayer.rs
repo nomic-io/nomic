16675b2a4408a488c469154fa37c03b634dd661f
Judd <kepzorz@gmail.com>
2019-11-06 16:27:32 -0800

    configure bitcoin rpc client

--- relayer/src/relayer.rs
+++ relayer/src/relayer.rs
@@ -1,4 +1,6 @@
+use bitcoincore_rpc::{Auth, Client, RpcApi};
 use nomic_primitives::transaction::Transaction;
+use std::env;
 
 #[derive(Debug)]
 pub enum RelayerState {
@@ -23,20 +25,34 @@ impl RelayerState {
 
 pub struct RelayerStateMachine {
     pub state: RelayerState,
+    rpc: Client,
 }
 
 impl RelayerStateMachine {
     pub fn new() -> Self {
+        let rpc_user = env::var("BTC_RPC_USER").unwrap();
+        let rpc_pass = env::var("BTC_RPC_PASS").unwrap();
+        let rpc_auth = Auth::UserPass(rpc_user, rpc_pass);
+        let rpc_url = "http://localhost:18332";
         RelayerStateMachine {
             state: RelayerState::ScanningBitcoin,
+            rpc: Client::new(rpc_url.to_string(), rpc_auth).unwrap(),
         }
     }
 
     pub fn run(&mut self) -> RelayerEvent {
         match &mut self.state {
-            _ => RelayerEvent::NewHeader,
+            _ => {
+                get_best_hash(&self.rpc);
+                RelayerEvent::NewHeader
+            }
         }
     }
+}
+
+fn get_best_hash(rpc: &Client) {
+    let hash = &rpc.get_best_block_hash().unwrap();
+    println!("best hash: {}", hash);
 }
 
 #[cfg(test)]
@@ -46,7 +62,5 @@ mod tests {
     fn run_relayer_state_machine() {
         let mut sm = RelayerStateMachine::new();
         let event = sm.run();
-
-        println!("got an event: {:?}", event);
     }
 }
