c31dae1d28da228546a3ffa293b16925bd7e28d9
Judd <kepzorz@gmail.com>
2019-11-07 14:28:27 -0800

    add relayer bitcoin rpc initialization

--- relayer/src/relayer.rs
+++ relayer/src/relayer.rs
@@ -1,4 +1,4 @@
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoincore_rpc::{Auth, Client, Error, RpcApi};
 use nomic_primitives::transaction::Transaction;
 use std::env;
 
@@ -53,36 +53,49 @@ impl RelayerState {
             (BroadcastHeaderTransaction, BroadcastHeaderTransactionFailure) => {
                 BroadcastHeaderTransaction
             }
-            (s, e) => Failure,
+            (_, _) => Failure,
         }
     }
 }
 
 pub struct RelayerStateMachine {
     pub state: RelayerState,
-    rpc: Client,
+    rpc: Option<Client>,
 }
 
 impl RelayerStateMachine {
     pub fn new() -> Self {
-        let rpc_user = env::var("BTC_RPC_USER").unwrap();
-        let rpc_pass = env::var("BTC_RPC_PASS").unwrap();
-        let rpc_auth = Auth::UserPass(rpc_user, rpc_pass);
-        let rpc_url = "http://localhost:18332";
         RelayerStateMachine {
             state: RelayerState::InitializeBitcoinRpc,
-            rpc: Client::new(rpc_url.to_string(), rpc_auth).unwrap(),
+            rpc: None,
         }
     }
 
     pub fn run(&mut self) -> RelayerEvent {
+        use self::RelayerEvent::*;
+        use self::RelayerState::*;
         match &mut self.state {
-            _ => {
-                get_best_hash(&self.rpc);
-                RelayerEvent::InitializeBitcoinRpcSuccess
+            InitializeBitcoinRpc => {
+                let rpc = make_rpc_client();
+                match rpc {
+                    Ok(rpc) => {
+                        self.rpc = Some(rpc);
+                        InitializeBitcoinRpcSuccess
+                    }
+                    Err(_) => InitializeBitcoinRpcFailure,
+                }
             }
+            _ => panic!("Relayer is in an unhandled state"),
         }
     }
+}
+
+pub fn make_rpc_client() -> Result<Client, Error> {
+    let rpc_user = env::var("BTC_RPC_USER").unwrap();
+    let rpc_pass = env::var("BTC_RPC_PASS").unwrap();
+    let rpc_auth = Auth::UserPass(rpc_user, rpc_pass);
+    let rpc_url = "http://localhost:18332";
+    Client::new(rpc_url.to_string(), rpc_auth)
 }
 
 fn get_best_hash(rpc: &Client) {
@@ -98,5 +111,6 @@ mod tests {
         let mut sm = RelayerStateMachine::new();
         let event = sm.run();
         sm.state = sm.state.next(event);
+        println!("sm state: {:?}", sm.state);
     }
 }
