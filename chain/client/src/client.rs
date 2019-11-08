7b0de2850ca87ad0f6f301360424b598c6022755
Judd <kepzorz@gmail.com>
2019-11-07 19:03:01 -0800

    mock client interface

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -1,12 +1,26 @@
-pub struct Client {}
+use bitcoin::hashes::sha256d::Hash;
+
+pub struct Client {
+    bitcoin_block_hashes: Vec<Hash>,
+}
 
 impl Client {
     pub fn new() -> Result<Self, ClientError> {
-        Ok(Client {})
+        Ok(Client {
+            bitcoin_block_hashes: Vec::new(),
+        })
     }
 
     /// Get the Bitcoin headers currently used by the peg zone's on-chain SPV client.
-    pub fn get_bitcoin_headers() {}
+    pub fn get_bitcoin_block_hashes(&self) -> Result<Vec<Hash>, ClientError> {
+        Ok(self.bitcoin_block_hashes.clone())
+    }
+
+    /// Set the peg's headers. This is only for use in testing since this is currently a mock
+    /// client.
+    pub fn set_bitcoin_block_hashes(&mut self, bitcoin_block_hashes: Vec<Hash>) {
+        self.bitcoin_block_hashes = bitcoin_block_hashes;
+    }
 }
 
 pub struct ClientError {}
