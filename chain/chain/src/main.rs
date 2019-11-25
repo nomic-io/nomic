a152abbe406776df7e78f154e149b0a99448a855
Judd <kepzorz@gmail.com>
2019-11-25 13:53:10 -0800

    add main chain program

--- chain/chain/src/main.rs
+++ chain/chain/src/main.rs
@@ -1,5 +1,67 @@
 //!  Start the peg abci server.
 
-use state_machine::{initialize, run};
+use abci2::messages::abci::*;
+use error_chain::bail;
+use merk::Merk;
+use nomic_chain::state_machine::{initialize, run};
+use nomic_chain::Action;
+use nomic_primitives::transaction::Transaction;
+use orga::abci::{ABCIStateMachine, Application};
+use orga::Result as OrgaResult;
+use orga::{MerkStore, Store};
 
-pub fn main() {}
+struct App;
+
+impl Application for App {
+    fn init_chain(
+        &self,
+        store: &mut dyn Store,
+        req: RequestInitChain,
+    ) -> OrgaResult<ResponseInitChain> {
+        println!("init chain: {:?}", req);
+        initialize(store);
+
+        Ok(ResponseInitChain::new())
+    }
+
+    fn check_tx(&self, store: &mut dyn Store, req: RequestCheckTx) -> OrgaResult<ResponseCheckTx> {
+        let tx = serde_json::from_slice::<Transaction>(req.get_tx());
+
+        match tx {
+            Ok(tx) => match run(store, Action::Transaction(tx)) {
+                Ok(execution_result) => Ok(Default::default()),
+
+                Err(e) => bail!("error executing tx (check_tx)"),
+            },
+
+            Err(e) => bail!("error deserializing tx (check_tx)"),
+        }
+    }
+
+    fn deliver_tx(
+        &self,
+        store: &mut dyn Store,
+        req: RequestDeliverTx,
+    ) -> OrgaResult<ResponseDeliverTx> {
+        let tx = serde_json::from_slice::<Transaction>(req.get_tx());
+
+        match tx {
+            Ok(tx) => match run(store, Action::Transaction(tx)) {
+                Ok(execution_result) => Ok(Default::default()),
+
+                Err(e) => bail!("error executing tx (deliver_tx)"),
+            },
+            Err(e) => bail!("error deserializing tx (deliver_tx)"),
+        }
+    }
+}
+
+pub fn main() {
+    let mut merk = Merk::open("./merk.db").unwrap();
+    merk.destroy().unwrap();
+    let mut merk = Merk::open("./merk.db").unwrap();
+    let store = MerkStore::new(&mut merk);
+    ABCIStateMachine::new(App, store)
+        .listen("127.0.0.1:26658")
+        .unwrap();
+}
