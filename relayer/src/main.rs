a7309a250c5fbc9b78cec7b12d68c433f94a9a10
Judd <kepzorz@gmail.com>
2019-11-26 19:47:45 -0800

    add relayer main fn

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -1,3 +1,21 @@
+use bitcoin::hashes::sha256d::Hash;
+use relayer::relayer::{RelayerEvent, RelayerState, RelayerStateMachine};
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sm = RelayerStateMachine::new();
+    let mut latest_tip: Option<Hash> = None;
+    loop {
+        let event = sm.run();
+
+        if let RelayerEvent::ComputeCommonAncestorSuccess { common_block_hash } = event {
+            if Some(common_block_hash) != latest_tip && latest_tip.is_some() {
+                println!("New tip hash: {:?}", common_block_hash);
+            } else {
+                thread::sleep(time::Duration::from_secs(10));
+            }
+            latest_tip = Some(common_block_hash);
+        }
+        sm.state = sm.state.next(event);
+    }
 }
