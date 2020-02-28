3ee07420a5aebe8b5838ef159e1535d1c09b76f1
Judd <kepzorz@gmail.com>
2020-02-27 17:47:53 -0800

    add nomic-cli and relayer subcommand

---
+++ cli/src/main.rs
@@ -0,0 +1,38 @@
+use clap::Clap;
+
+/// Command-line interface for interacting with the Nomic Bitcoin sidechain
+#[derive(Clap)]
+#[clap(version = "0.1.0")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    /// Relays data between a local Bitcoin full node and the sidechain
+    #[clap(name = "relayer", version = "0.1.0")]
+    Relayer(Relayer),
+
+    /// Starts a sidechain full node
+    #[clap(name = "start")]
+    Start(Start),
+}
+
+#[derive(Clap)]
+struct Start {}
+
+#[derive(Clap)]
+struct Relayer {}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Relayer(r) => {
+            relayer::relayer::start();
+        }
+        SubCommand::Start(s) => {
+            println!("running start");
+        }
+    }
+}
