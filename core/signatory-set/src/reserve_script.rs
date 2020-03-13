4b9ec3f65de55ced72eba9b169026b2f199e63ba
Matt Bell <mappum@gmail.com>
2020-03-12 19:05:02 -0700

    Added data field in reserve script

--- core/signatory-set/src/reserve_script.rs
+++ core/signatory-set/src/reserve_script.rs
@@ -2,7 +2,7 @@ use super::{Signatory, SignatorySet};
 use bitcoin_script::bitcoin_script as script;
 use nomic_bitcoin::bitcoin::Script;
 
-pub fn redeem_script(signatories: &SignatorySet) -> Script {
+pub fn redeem_script(signatories: &SignatorySet, data: Vec<u8>) -> Script {
     let first_signatory_script = |signatory: &Signatory| script! {
         <signatory.pubkey> OP_CHECKSIG
         OP_IF
@@ -22,7 +22,11 @@ pub fn redeem_script(signatories: &SignatorySet) -> Script {
 
     let greater_than_script = |n: u32| script! {
         <n as i64> OP_GREATERTHAN
+        OP_VERIFY
     };
+
+    // put a 1 at the end so nobody can make an unspendable output
+    let data_script = |data: Vec<u8>| script!(<data> 1);
 
     let mut iter = signatories.iter();
 
@@ -39,21 +43,13 @@ pub fn redeem_script(signatories: &SignatorySet) -> Script {
     let two_thirds = signatories.total_voting_power() as u64 * 2 / 3;
     bytes.extend(&greater_than_script(two_thirds as u32).into_bytes());
 
+    bytes.extend(&data_script(data).into_bytes());
+
     bytes.into()
 }
 
-pub fn output_script(signatories: &SignatorySet) -> Script {
-    redeem_script(signatories).to_v0_p2wsh()
-}
-
-impl SignatorySet {
-    pub fn to_redeem_script(&self) -> Script {
-        redeem_script(self)
-    }
-
-    pub fn to_output_script(&self) -> Script {
-        output_script(self)
-    }
+pub fn output_script(signatories: &SignatorySet, data: Vec<u8>) -> Script {
+    redeem_script(signatories, data).to_v0_p2wsh()
 }
 
 #[cfg(test)]
@@ -65,12 +61,18 @@ mod tests {
     #[test]
     #[should_panic(expected = "Cannot build script for empty signatory set")]
     fn redeem_script_empty() {
-        redeem_script(&SignatorySet::new());
+        redeem_script(
+            &SignatorySet::new(),
+            vec![1, 2, 3]
+        );
     }
 
     #[test]
     fn redeem_script_fixture() {
-        let script = mock_signatory_set(4).to_redeem_script();
+        let script = redeem_script(
+            &mock_signatory_set(4),
+            vec![1, 2, 3]
+        );
 
         assert_eq!(
             script,
@@ -101,38 +103,36 @@ mod tests {
                 OP_ENDIF
 
                 6 OP_GREATERTHAN
+                OP_VERIFY
+
+                0x010203 1
             }
         );
 
         assert_eq!(
             script.into_bytes(),
             vec![
-                33, 3, 70, 39, 121, 173, 74, 173, 57, 81, 70, 20, 117, 26, 113, 8, 95, 47, 16, 225,
-                199, 165, 147, 228, 224, 48, 239, 181, 184, 114, 28, 229, 91, 11, 172, 99, 84, 103,
-                0, 104, 124, 33, 2, 83, 31, 230, 6, 129, 52, 80, 61, 39, 35, 19, 50, 39, 200, 103,
-                172, 143, 166, 200, 60, 83, 126, 154, 68, 195, 197, 189, 189, 203, 31, 227, 55,
-                172, 99, 83, 147, 104, 124, 33, 2, 77, 75, 108, 209, 54, 16, 50, 202, 155, 210,
-                174, 185, 217, 0, 170, 77, 69, 217, 234, 216, 10, 201, 66, 51, 116, 196, 81, 167,
-                37, 77, 7, 102, 172, 99, 82, 147, 104, 124, 33, 3, 27, 132, 197, 86, 123, 18, 100,
-                64, 153, 93, 62, 213, 170, 186, 5, 101, 215, 30, 24, 52, 96, 72, 25, 255, 156, 23,
-                245, 233, 213, 221, 7, 143, 172, 99, 81, 147, 104, 86, 160
+                33, 3, 70, 39, 121, 173, 74, 173, 57, 81, 70, 20, 117, 26, 113, 8, 95, 47, 16, 225, 199, 165, 147, 228, 224, 48, 239, 181, 184, 114, 28, 229, 91, 11, 172, 99, 84, 103, 0, 104, 124, 33, 2, 83, 31, 230, 6, 129, 52, 80, 61, 39, 35, 19, 50, 39, 200, 103, 172, 143, 166, 200, 60, 83, 126, 154, 68, 195, 197, 189, 189, 203, 31, 227, 55, 172, 99, 83, 147, 104, 124, 33, 2, 77, 75, 108, 209, 54, 16, 50, 202, 155, 210, 174, 185, 217, 0, 170, 77, 69, 217, 234, 216, 10, 201, 66, 51, 116, 196, 81, 167, 37, 77, 7, 102, 172, 99, 82, 147, 104, 124, 33, 3, 27, 132, 197, 86, 123, 18, 100, 64, 153, 93, 62, 213, 170, 186, 5, 101, 215, 30, 24, 52, 96, 72, 25, 255, 156, 23, 245, 233, 213, 221, 7, 143, 172, 99, 81, 147, 104, 86, 160, 105, 3, 1, 2, 3, 81
             ]
         );
     }
 
     #[test]
     fn output_script_fixture() {
-        let script = mock_signatory_set(4).to_output_script();
+        let script = output_script(
+            &mock_signatory_set(4),
+            vec![1, 2, 3]
+        );
 
         assert_eq!(
             script,
             bitcoin_script! {
-                0 0xc3cc0f5ae30d5da678fc042d46fcf0c1203f8a0de604f1165ceda9e00afb50f2
+                0 0x7d7aa8c8120655f0419912c34dbd3d8c0d1eff5e266eaacee267a3c3b5cf4f0b
             }
         );
         assert_eq!(
             script.into_bytes(),
-            vec![0, 32, 195, 204, 15, 90, 227, 13, 93, 166, 120, 252, 4, 45, 70, 252, 240, 193, 32, 63, 138, 13, 230, 4, 241, 22, 92, 237, 169, 224, 10, 251, 80, 242]
+            vec![0, 32, 125, 122, 168, 200, 18, 6, 85, 240, 65, 153, 18, 195, 77, 189, 61, 140, 13, 30, 255, 94, 38, 110, 170, 206, 226, 103, 163, 195, 181, 207, 79, 11]
         );
     }
 }
