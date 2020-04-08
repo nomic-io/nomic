a52ff7ab4d2942d92006681354859030572bed46
Judd Keppel <kepzorz@gmail.com>
2020-04-07 17:47:50 -0700

    Added verify_signature method to TransferTransaction

--- core/primitives/src/transaction.rs
+++ core/primitives/src/transaction.rs
@@ -4,6 +4,7 @@ use bitcoin::BlockHeader;
 use nomic_bitcoin::bitcoin;
 use serde::{de::Deserializer, ser::SerializeSeq, Deserialize, Serialize, Serializer};
 use crate::error::Result;
+use secp256k1::{Secp256k1, VerifyOnly};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Transaction {
@@ -82,6 +83,18 @@ impl TransferTransaction {
         let mut hasher = Sha256::new();
         hasher.input(self.sighash_input()?.as_slice());
         Ok(hasher.result().to_vec())
+    }
+
+    pub fn verify_signature(&self, secp: &Secp256k1<VerifyOnly>) -> Result<bool> {
+        use secp256k1::{Message, PublicKey, Signature};
+        let sighash = Message::from_slice(self.sighash()?.as_slice())?;
+        let signature = Signature::from_compact(self.signature.as_slice())?;
+        let pubkey = PublicKey::from_slice(self.from.as_slice())?;
+        if let Err(_) = secp.verify(&sighash, &signature, &pubkey) {
+            Ok(false)
+        } else {
+            Ok(true)
+        }
     }
 }
 
