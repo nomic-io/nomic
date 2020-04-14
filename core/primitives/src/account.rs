41dbc9237a0fe7f8c8562b65e3f8085a3a41f0c3
Judd <kepzorz@gmail.com>
2020-04-14 16:15:12 -0700

    add account encoding/decoding
    Co-authored-by: Matt Bell <mappum@gmail.com>

--- core/primitives/src/account.rs
+++ core/primitives/src/account.rs
@@ -1,30 +1,29 @@
 use crate::Result;
-use orga::Store;
-use serde::{Deserialize, Serialize};
+use orga::{Decode, Encode, Store};
+use std::io::{Read, Write};
 
-#[derive(Debug, Serialize, Deserialize, Default, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Account {
     pub nonce: u64,
     pub balance: u64,
 }
 
-fn store_key(address: &[u8]) -> Vec<u8> {
-    [b"accounts/", address].concat()
-}
-
-impl Account {
-    pub fn get(store: &mut dyn Store, address: &[u8]) -> Result<Option<Account>> {
-        let account_key = store_key(address);
-        let maybe_account = store
-            .get(account_key.as_slice())?
-            .map(|account_bytes| bincode::deserialize(account_bytes.as_slice()))
-            .transpose()?;
-        Ok(maybe_account)
+impl Encode for Account {
+    fn encode_into<W: Write>(&self, dest: &mut W) -> Result<()> {
+        self.nonce.encode_into(dest)?;
+        self.balance.encode_into(dest)
     }
 
-    pub fn set(store: &mut dyn Store, address: &[u8], account: Account) -> Result<()> {
-        let account_key = store_key(address);
-        let account_bytes = bincode::serialize(&account)?;
-        store.put(account_key, account_bytes)
+    fn encoding_length(&self) -> Result<usize> {
+        Ok(self.nonce.encoding_length()? + self.balance.encoding_length()?)
+    }
+}
+
+impl Decode for Account {
+    fn decode<R: Read>(mut input: R) -> Result<Self> {
+        Ok(Self {
+            nonce: u64::decode(&mut input)?,
+            balance: u64::decode(&mut input)?,
+        })
     }
 }
