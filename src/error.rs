6d60059090fe46c945f2026bfbe7dd1eb388d167
Charlie Little <cwlittle@utexas.edu>
2021-10-30 05:36:13 -0500

    Add primitive error types

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Header(String),
+    #[error(transparent)]
+    Orga(#[from] orga::Error),
+    #[error("Unknown Error")]
+    Unknown,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
