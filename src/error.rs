15e6727d04f758d41a3562b6b8d6650eda7037ad
Charlie Little <cwlittle@utexas.edu>
2021-11-09 20:15:14 -0600

    Add transparent Bitcoin error type

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error(transparent)]
+    Bitcoin(#[from] bitcoin::util::Error),
     #[error("{0}")]
     Header(String),
     #[error(transparent)]
