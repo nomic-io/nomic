d22ab26e39506367d82528d35cd5cc915171a1dd
Matt Bell <mappum@gmail.com>
2022-01-17 20:06:33 -0600

    Configure for release candidate network

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,3 @@
-#[cfg(full)]
-use crate::bitcoin::header_queue::HeaderQueue;
 use orga::prelude::*;
 
 #[derive(State, Debug, Clone)]
@@ -10,18 +8,22 @@ impl Symbol for Gucci {}
 pub struct InnerApp {
     pub accounts: Accounts<Gucci>,
     pub staking: Staking<Gucci>,
-    // pub btc_headers: HeaderQueue,
 }
 
 #[cfg(feature = "full")]
 impl InitChain for InnerApp {
     fn init_chain(&mut self, _ctx: &InitChainCtx) -> Result<()> {
+        self.staking.set_min_self_delegation(100_000);
+        self.staking.set_max_validators(3);
+        self.accounts.allow_transfers(true);
+
         self.accounts.deposit(
-            "nomic18m73q0542w8vlt0s23sazy62hq6tfnl9pjpslh"
+            "nomic1ns0gwwx7pp0f3gdhal5t77msvdkj6trgu2mdek"
                 .parse()
                 .unwrap(),
             100_000_000_000.into(),
         )?;
+
         Ok(())
     }
 }
@@ -32,12 +34,19 @@ impl BeginBlock for InnerApp {
         self.staking.begin_block(ctx)?;
 
         if self.staking.staked()? > 0 {
-            let divisor: Amount = 50_000.into();
+            let divisor: Amount = 100_000.into();
             let reward = (self.staking.staked()? / divisor)?.amount()?;
             self.staking.give(reward.into())?;
         }
 
         Ok(())
+    }
+}
+
+#[cfg(feature = "full")]
+impl EndBlock for InnerApp {
+    fn end_block(&mut self, ctx: &EndBlockCtx) -> Result<()> {
+        self.staking.end_block(ctx)
     }
 }
 
