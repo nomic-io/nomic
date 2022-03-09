2a30873cbe48e6795dc444894250b063a71124ff
Matt Bell <mappum@gmail.com>
2022-03-08 18:29:46 -0600

    Use built-in genesis in start command

--- src/main.rs
+++ src/main.rs
@@ -88,10 +88,9 @@ impl StartCmd {
     async fn run(&self) -> Result<()> {
         tokio::task::spawn_blocking(|| {
             Node::<app::App>::new(CHAIN_ID)
-                // .with_genesis(include_bytes!("../genesis.json"))
+                .with_genesis(include_bytes!("../genesis.json"))
                 .stdout(std::process::Stdio::inherit())
                 .stderr(std::process::Stdio::inherit())
-                .reset()
                 .run()
         })
         .await
