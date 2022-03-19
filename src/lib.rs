e1680f0cc92a581a68a2849c3d67d801c0d4e9e7
Judd Keppel <kepzorz@gmail.com>
2022-03-18 20:20:44 -0500

    Expose command

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@
 #![feature(never_type)]
 #![allow(incomplete_features)]
 #![feature(specialization)]
+#![allow(incomplete_features)]
+#![feature(async_closure)]
 
 #[cfg(feature = "full")]
 use orga::abci::TendermintClient;
 
 pub use orga;
+
+pub mod command;
 
 pub mod app;
 // pub mod bitcoin;
