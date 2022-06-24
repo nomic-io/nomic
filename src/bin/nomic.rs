4404805c5632bbff992505d77c78809d392b1d9c
Matt Bell <mappum@gmail.com>
2022-06-23 22:58:43 -0500

    Update stop time

--- src/bin/nomic.rs
+++ src/bin/nomic.rs
@@ -16,7 +16,7 @@ use orga::prelude::*;
 use serde::{Deserialize, Serialize};
 use tendermint_rpc::Client as _;
 
-const STOP_SECONDS: i64 = 1656003600;
+const STOP_SECONDS: i64 = 1656090000;
 const STATE_SYNC_DELAY: i64 = 3 * orga::merk::store::SNAPSHOT_INTERVAL as i64;
 
 fn now_seconds() -> i64 {
