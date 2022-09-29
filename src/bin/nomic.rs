5b7dee09b7228e797fda1d2f4d1f1686df4c0949
Matt Bell <mappum@gmail.com>
2022-09-29 16:12:32 -0500

    Update claim-airdrop CLI command, and add airdrop subcommand to view state

--- src/bin/nomic.rs
+++ src/bin/nomic.rs
@@ -65,6 +65,7 @@ pub enum Command {
     Unjail(UnjailCmd),
     Edit(EditCmd),
     Claim(ClaimCmd),
+    Airdrop(AirdropCmd),
     ClaimAirdrop(ClaimAirdropCmd),
     Relayer(RelayerCmd),
     Signer(SignerCmd),
@@ -97,6 +98,7 @@ impl Command {
             Edit(cmd) => cmd.run().await,
             Claim(cmd) => cmd.run().await,
             ClaimAirdrop(cmd) => cmd.run().await,
+            Airdrop(cmd) => cmd.run().await,
             // Legacy(cmd) => cmd.run().await,
             Relayer(cmd) => cmd.run().await,
             Signer(cmd) => cmd.run().await,
@@ -670,15 +672,94 @@ impl ClaimCmd {
 }
 
 #[derive(Parser, Debug)]
-pub struct ClaimAirdropCmd;
+pub struct AirdropCmd {
+    address: Option<Address>,
+}
+
+impl AirdropCmd {
+    async fn run(&self) -> Result<()> {
+        let client = app_client();
+
+        let addr = self.address.unwrap_or_else(|| my_address());
+        let acct = match client.airdrop.get(addr).await?? {
+            None => {
+                println!("Address is not eligible for airdrop");
+                return Ok(())
+            }
+            Some(acct) => acct,
+        };
+
+        println!("{:#?}", acct);
+
+        Ok(())
+    }
+}
+
+#[derive(Parser, Debug)]
+pub struct ClaimAirdropCmd {
+    address: Option<Address>,
+}
 
 impl ClaimAirdropCmd {
     async fn run(&self) -> Result<()> {
-        Ok(app_client()
-            .pay_from(async move |client| client.atom_airdrop.claim().await)
-            .accounts
-            .give_from_funding_all()
-            .await?)
+        let client = app_client();
+
+        let addr = self.address.unwrap_or_else(|| my_address());
+        let acct = match client.airdrop.get(addr).await?? {
+            None => {
+                println!("Address is not eligible for airdrop");
+                return Ok(())
+            }
+            Some(acct) => acct,
+        };
+
+        let mut claimed = false;
+
+        if acct.airdrop1.claimable > 0 {
+            app_client()
+                .pay_from(async move |client| client.airdrop.claim_airdrop1().await)
+                .accounts
+                .give_from_funding_all()
+                .await?;
+            println!("Claimed airdrop 1 ({} uNOM)", acct.airdrop1.claimable);
+            claimed = true;
+        }
+
+        if acct.btc_deposit.claimable > 0 {
+            app_client()
+                .pay_from(async move |client| client.airdrop.claim_btc_deposit().await)
+                .accounts
+                .give_from_funding_all()
+                .await?;
+            println!("Claimed BTC deposit airdrop ({} uNOM)", acct.btc_deposit.claimable);
+            claimed = true;
+        }
+
+        if acct.btc_withdraw.claimable > 0 {
+            app_client()
+                .pay_from(async move |client| client.airdrop.claim_btc_withdraw().await)
+                .accounts
+                .give_from_funding_all()
+                .await?;
+            println!("Claimed BTC withdraw airdrop ({} uNOM)", acct.btc_withdraw.claimable);
+            claimed = true;
+        }
+
+        if acct.ibc_transfer.claimable > 0 {
+            app_client()
+                .pay_from(async move |client| client.airdrop.claim_ibc_transfer().await)
+                .accounts
+                .give_from_funding_all()
+                .await?;
+            println!("Claimed IBC transfer airdrop ({} uNOM)", acct.ibc_transfer.claimable);
+            claimed = true;
+        }
+
+        if !claimed {
+            println!("No claimable airdrops");
+        }
+
+        Ok(())
     }
 }
 
