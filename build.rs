ec3a34ec48e037c9ce1880624caa08d5a321b58b
Matt Bell <mappum@gmail.com>
2023-07-27 17:00:22 -0500

    Get legacy binary rev from network config

--- build.rs
+++ build.rs
@@ -3,25 +3,36 @@ fn main() {
         .args(["symbolic-ref", "--short", "HEAD"])
         .output()
         .unwrap();
-    println!(
-        "cargo:rustc-env=GIT_BRANCH={}",
-        String::from_utf8(branch_name.stdout).unwrap().trim(),
-    );
+    let branch_name = String::from_utf8(branch_name.stdout)
+        .unwrap()
+        .trim()
+        .to_string();
+    println!("cargo:rustc-env=GIT_BRANCH={}", branch_name);
 
     #[cfg(feature = "legacy-bin")]
     {
         println!("cargo:rerun-if-changed=build.sh");
-
         let shell = std::env::var("SHELL").unwrap_or("/bin/bash".to_string());
         println!("using shell: {}", shell);
-
-        // TODO: decide how to configure rev (support multiple?)
-        let legacy_rev = "testnet".to_string();
+        let version = if branch_name == "main" {
+            todo!()
+        } else {
+            let toml = include_str!("networks/testnet.toml");
+            let config: toml::Value = toml::from_str(toml).unwrap();
+            config
+                .as_table()
+                .unwrap()
+                .get("legacy_version")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string()
+        };
 
         std::process::Command::new(shell)
             .env_clear()
             .env("OUT_DIR", std::env::var("OUT_DIR").unwrap())
-            .env("NOMIC_LEGACY_REV", legacy_rev)
+            .env("NOMIC_LEGACY_REV", format!("v{}", version))
             .args(["build.sh"])
             .spawn()
             .unwrap()
