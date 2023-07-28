44956ed3a80710c518c57db875c69f1097a46d5a
Matt Bell <mappum@gmail.com>
2023-07-28 15:06:32 -0500

    Remove nomic legacy build dir after build

--- build.sh
+++ build.sh
@@ -3,18 +3,27 @@
 set -e
 
 BUILD_DIR=$OUT_DIR/nomic
-echo "Building legacy nomic at $BUILD_DIR..."
-if [ ! -d "$BUILD_DIR" ]; then
-    git clone https://github.com/nomic-io/nomic.git $OUT_DIR/nomic
-fi
-cd $BUILD_DIR
-git checkout .
-git checkout main
-git pull
-git checkout $NOMIC_LEGACY_REV
+NOMIC_LEGACY_PATH=$OUT_DIR/nomic-bin
 
-rustc --version
-cargo build --release
-NOMIC_LEGACY_PATH=$OUT_DIR/nomic/target/release/nomic
+if [ ! -f "$NOMIC_LEGACY_PATH" ]; then
+    echo "Building legacy nomic at $BUILD_DIR..."
+    if [ ! -d "$BUILD_DIR" ]; then
+        git clone https://github.com/nomic-io/nomic.git $BUILD_DIR
+    fi
+    cd $BUILD_DIR
+    git checkout .
+    git checkout main
+    git pull
+    git checkout $NOMIC_LEGACY_REV
+
+    rustc --version
+    cargo build --release
+    mv $BUILD_DIR/target/release/nomic $NOMIC_LEGACY_PATH
+else
+    echo "Skipping legacy nomic binary build (already exists at $NOMIC_LEGACY_PATH)" 
+fi
+
+rm -rf $BUILD_DIR
+
 echo "cargo:rustc-env=NOMIC_LEGACY_BUILD_PATH=$NOMIC_LEGACY_PATH"
 echo "cargo:rustc-env=NOMIC_LEGACY_BUILD_VERSION=$($NOMIC_LEGACY_PATH --version)"
