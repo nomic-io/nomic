510d6c6051dfb7dc95a77b472cb15861ad8e708b
Matt Bell <mappum@gmail.com>
2024-02-13 19:10:06 -0600

    Increase default checkpoint max_age to 90 days

--- src/bitcoin/checkpoint.rs
+++ src/bitcoin/checkpoint.rs
@@ -1086,7 +1086,7 @@ impl Config {
             max_checkpoint_interval: 60 * 60 * 8,
             max_inputs: 40,
             max_outputs: 200,
-            max_age: 60 * 60 * 24 * 7 * 3,
+            max_age: 60 * 60 * 24 * 30 * 3,
             target_checkpoint_inclusion: 2,
             min_fee_rate: 2, // relay threshold is 1 sat/vbyte
             max_fee_rate: 200,
