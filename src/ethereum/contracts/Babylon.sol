878d4b9cd3bf38efe70dcf067623c444e9bb658a
Matt Bell <mappum@gmail.com>
2024-11-06 16:48:10 -0600

    Update Babylon staking contract for updated 'stake' and 'unstake' dests

--- src/ethereum/contracts/Babylon.sol
+++ src/ethereum/contracts/Babylon.sol
@@ -11,9 +11,8 @@ import "./Nomic.sol";
 contract Babylon is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    bytes32 public immutable state_id;
-    address public immutable state_tokenContract;
     address public immutable state_nomicContract;
+    address public immutable state_tokenContract;
 
     uint256 public state_delegations = 0;
     mapping(uint256 => address) public state_owners;
@@ -21,8 +20,7 @@ contract Babylon is ReentrancyGuard {
     function stake(
         uint256 amount,
         bytes32 finalityProvider,
-        uint16 stakingPeriod,
-        uint16 unbondingPeriod
+        uint16 stakingPeriod
     ) external returns (uint256) {
         IERC20(state_tokenContract).safeTransferFrom(
             msg.sender,
@@ -32,14 +30,18 @@ contract Babylon is ReentrancyGuard {
 
         string memory dest = string.concat(
             '{"type":"stake","owner":"',
-            Strings.toHexString(uint256(uint160(msg.sender)), 20),
+            Strings.toHexString(uint256(uint160(address(this))), 20),
             '","finality_provider":"',
             Strings.toHexString(uint256(finalityProvider), 32),
             '","staking_period":',
             Strings.toString(stakingPeriod),
-            ',"unbonding_period":',
-            Strings.toString(unbondingPeriod),
-            "}"
+            ',"return_dest":"{\\"type\\":\\"ethAccount\\",\\"address\\":\\"',
+            Strings.toHexString(uint256(uint160(msg.sender)), 20),
+            '\\",\\"connection\\":\\"',
+            Strings.toHexString(uint256(uint160(state_nomicContract)), 20),
+            '\\",\\"network\\":',
+            Strings.toString(block.chainid),
+            '}"}'
         );
 
         Nomic(state_nomicContract).sendToNomic(
@@ -62,7 +64,7 @@ contract Babylon is ReentrancyGuard {
         );
 
         string memory dest = string.concat(
-            '{"type":"unstake","index":"',
+            '{"type":"unstake","index":',
             Strings.toString(index),
             "}"
         );
@@ -70,10 +72,9 @@ contract Babylon is ReentrancyGuard {
         Nomic(state_nomicContract).sendToNomic(state_tokenContract, dest, 0);
     }
 
-    constructor(bytes32 _id, address _tokenContract, address _nomicContract) {
-        state_id = _id;
-        state_tokenContract = _tokenContract;
+    constructor(address _nomicContract, address _tokenContract) {
         state_nomicContract = _nomicContract;
+        state_tokenContract = _tokenContract;
 
         IERC20(_tokenContract).approve(_nomicContract, type(uint256).max);
     }
