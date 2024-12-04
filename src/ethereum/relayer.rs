e3b29b4b485138ccf6dea5205c0f19d7e450c699
Matt Bell <mappum@gmail.com>
2024-12-04 17:41:01 -0600

    Use Holesky chainid in Ethereum relayer

--- src/ethereum/relayer.rs
+++ src/ethereum/relayer.rs
@@ -353,13 +353,13 @@ impl<
         let rpc_client =
             crate::ethereum::consensus::relayer::RpcClient::new(beacon_api_url.clone());
         // TODO: use chain_id in closure without breaking fn coercion
-        let lc = client.sub(move |app: InnerApp| Ok(app.ethereum.light_client(11155111)?));
+        let lc = client.sub(move |app: InnerApp| Ok(app.ethereum.light_client(17000)?));
         let updates = crate::ethereum::consensus::relayer::get_updates(&lc, &rpc_client).await?;
 
         for update in updates {
             log::info!(
                 "Relaying Ethereum consensus update... (chainid={}, slot={})",
-                11155111, // TODO: self.eth_chainid,
+                17000, // TODO: self.eth_chainid,
                 update.finalized_header.beacon.slot
             );
 
